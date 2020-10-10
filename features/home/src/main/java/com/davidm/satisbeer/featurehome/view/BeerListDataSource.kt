d505e7f12148ac74bb19ec1f0bb0f6a458bc34d3
David <david.marinangeli@gmail.com>
2020-10-10 21:11:01 +0400

    Fix page loader

--- features/home/src/main/java/com/davidm/satisbeer/featurehome/view/BeerListDataSource.kt
+++ features/home/src/main/java/com/davidm/satisbeer/featurehome/view/BeerListDataSource.kt
@@ -2,38 +2,33 @@ package com.davidm.satisbeer.featurehome.view
 
 import android.util.Log
 import androidx.paging.PageKeyedDataSource
-import kotlinx.coroutines.*
 import com.davidm.satisbeer.featurehome.data.Beer
 import com.davidm.satisbeer.featurehome.repository.HomeRepository
+import kotlinx.coroutines.*
 
+@ExperimentalCoroutinesApi
 class BeerListDataSource(
-    private val coroutineScope: CoroutineScope,
+    coroutineScope: CoroutineScope,
     private val homeRepository: HomeRepository
 ) : PageKeyedDataSource<Int, Beer>() {
 
-    var page = 1;
+    private val pageLoaderScope =
+        CoroutineScope(coroutineScope.newCoroutineContext(Dispatchers.Main))
 
     override fun loadInitial(
         params: LoadInitialParams<Int>,
         callback: LoadInitialCallback<Int, Beer>
     ) {
-        coroutineScope.launch {
+        pageLoaderScope.launch {
 
             try {
-                val result =
-                    homeRepository.retrieveBeers(page)
+                val result = homeRepository.retrieveBeers(params.requestedLoadSize, 1)
 
-                callback.onResult(
-                    result,
-                    page - 1,
-                    page + 1
-                )
-
+                callback.onResult(result, null, 2)
 
             } catch (exception: Exception) {
-                Log.e("loadInitialRedditData", exception.toString())
+                Log.e("BeerListDataSource", "loadInitialRedditData", exception)
             }
-
 
         }
     }
@@ -42,51 +37,49 @@ class BeerListDataSource(
         params: LoadParams<Int>,
         callback: LoadCallback<Int, Beer>
     ) {
-        coroutineScope.launch {
+        pageLoaderScope.launch {
 
             try {
-                val result =
-                    homeRepository.retrieveBeers(params.key)
+                val result = homeRepository.retrieveBeers(params.requestedLoadSize, params.key)
 
                 callback.onResult(
                     result,
-                    page + 1
+                    params.key + 1
                 )
 
             } catch (exception: Exception) {
-                Log.e("loadAfterRedditData", exception.toString())
+                Log.e("BeerListDataSource", "loadAfterRedditData", exception)
             }
 
-
-        }//To change body of created functions use File | Settings | File Templates.
+        }
     }
 
     override fun loadBefore(
         params: LoadParams<Int>,
         callback: LoadCallback<Int, Beer>
     ) {
-        coroutineScope.launch {
+        pageLoaderScope.launch {
 
             try {
                 val result =
                     withContext(Dispatchers.IO) {
-                        homeRepository.retrieveBeers(page)
+                        homeRepository.retrieveBeers(params.requestedLoadSize, params.key)
                     }
                 callback.onResult(
                     result,
-                    page - 1
+                    if (params.key == 1) null else params.key + 1
                 )
 
 
             } catch (exception: Exception) {
-                Log.e("loadBeforeRedditData", exception.toString())
+                Log.e("BeerListDataSource", "loadBeforeRedditData", exception)
             }
         }
     }
 
     override fun invalidate() {
+        pageLoaderScope.cancel()
         super.invalidate()
-        coroutineScope.cancel()
     }
 
 }
