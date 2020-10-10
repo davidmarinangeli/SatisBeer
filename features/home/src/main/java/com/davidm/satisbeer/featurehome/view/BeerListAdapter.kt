0b2179aec72af44c873d2f5d282e183fb1027b8b
David <david.marinangeli@gmail.com>
2020-10-10 20:09:56 +0100

    fixed UI

--- features/home/src/main/java/com/davidm/satisbeer/featurehome/view/BeerListAdapter.kt
+++ features/home/src/main/java/com/davidm/satisbeer/featurehome/view/BeerListAdapter.kt
@@ -9,47 +9,22 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.davidm.satisbeer.featurehome.data.Beer
 import com.davidm.satisbeer.featurehome.databinding.ItemBeerListBinding
-import com.davidm.satisbeer.featurehome.databinding.ItemWeekendOffersBinding
 import com.squareup.picasso.Picasso
 
 class BeerListAdapter : PagedListAdapter<Beer, RecyclerView.ViewHolder>(DiffUtilCallBack()) {
 
-    private val WEEKEND_OFFER_TYPE = 0
-    private val LIST_ITEM_TYPE = 1
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        return if (viewType == WEEKEND_OFFER_TYPE) {
-            val binding = ItemWeekendOffersBinding.inflate(LayoutInflater.from(parent.context))
-            WeekendOfferViewHolder(binding)
-        } else {
-            val binding = ItemBeerListBinding.inflate(LayoutInflater.from(parent.context))
-            ListItemViewHolder(binding)
-        }
 
+        val binding =
+            ItemBeerListBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ListItemViewHolder(binding)
     }
 
     override fun onBindViewHolder(viewHolder: RecyclerView.ViewHolder, position: Int) {
 
         val item = getItem(position) ?: return
-
-
-        if (getItemViewType(position) == WEEKEND_OFFER_TYPE) {
-            (viewHolder as WeekendOfferViewHolder).bind(item)
-
-        } else {
-            (viewHolder as ListItemViewHolder).bind(item)
-        }
+        (viewHolder as ListItemViewHolder).bind(item)
     }
-
-    override fun getItemViewType(position: Int): Int {
-        return if (position == 0) {
-            WEEKEND_OFFER_TYPE
-        } else {
-            LIST_ITEM_TYPE
-        }
-    }
-
-
 }
 
 class ListItemViewHolder(view: ItemBeerListBinding) : RecyclerView.ViewHolder(view.root) {
@@ -63,19 +38,6 @@ class ListItemViewHolder(view: ItemBeerListBinding) : RecyclerView.ViewHolder(vi
         beerSubtitle.text = beer.tagLine
         beerDescription.text = beer.description
         Picasso.get().load(beer.imageUrl).into(beerImage)
-    }
-}
-
-class WeekendOfferViewHolder(view: ItemWeekendOffersBinding) : RecyclerView.ViewHolder(view.root) {
-    val title: TextView = view.weekendOffersTitle
-    val subTitle: TextView = view.weekendOffersSubtitle
-
-    fun bind(item: Beer) {
-        //            (viewHolder as WeekendOfferViewHolder).title.text =
-//                Resources.getSystem().getText(R.string.weekend_offers_title_text)
-//
-//            viewHolder.subTitle.text =
-//                Resources.getSystem().getText(R.string.weekend_offer_subtitle_text)
     }
 }
 
@@ -95,5 +57,4 @@ class DiffUtilCallBack : DiffUtil.ItemCallback<Beer>() {
         return oldItem.name == newItem.name
                 && oldItem.description == newItem.description
     }
-
 }
